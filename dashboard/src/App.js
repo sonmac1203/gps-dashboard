d5eb3586b05947a11eecfe1ba537092f9f2eae6e
Son Mac <mactranthienson@gmail.com>
2023-02-14 21:12:55 -0700

    Create Dashboard component - Installed css-loader and style-loader - Create and export Dashboard component - Update App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,10 @@
+import { Dashboard } from '../components';
 function App() {
-  return <div className='App'>Hi</div>;
+  return (
+    <div className='App'>
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
