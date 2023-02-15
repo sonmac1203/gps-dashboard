3ef689cb2392dfd9bf2ccd6628225fa2a292d35a
Son Mac <mactranthienson@gmail.com>
2023-02-15 01:18:03 -0700

    Server: update broker.js

--- server/broker.js
+++ server/broker.js
@@ -4,61 +4,72 @@ const httpServer = require('http').createServer();
 const ws = require('websocket-stream');
 
 const MQTT_Port = 1884;
-const wsPort = 8884;
+const wsPort = 8000;
 
 server.listen(MQTT_Port, function () {
   console.log('Aedes MQTT server started and listening on port', MQTT_Port);
-}),
-  ws.createServer({ server: httpServer }, aedes.handle);
+});
+ws.createServer({ server: httpServer }, aedes.handle);
+
 httpServer.listen(wsPort, function () {
   console.log('websocket server listening on port ', wsPort);
 });
 
 // emitted when a client connects to the broker
 aedes.on('client', function (client) {
-  console.log(
-    `CLIENT_CONNECTED : MQTT Client ${
-      client ? client.id : client
-    } connected to aedes broker ${aedes.id}`
-  );
+  // console.log(
+  //   `CLIENT_CONNECTED : MQTT Client ${
+  //     client ? client.id : client
+  //   } connected to aedes broker ${aedes.id}`
+  // );
+  const clientId = client.id;
+  console.log('> Client connected');
+  aedes.publish({ topic: `gps/${clientId}`, payload: 'Connected' });
 });
 // emitted when a client disconnects from the broker
 aedes.on('clientDisconnect', function (client) {
-  console.log(
-    `CLIENT_DISCONNECTED : MQTT Client ${
-      client ? client.id : client
-    } disconnected from the aedes broker ${aedes.id}`
-  );
+  // console.log(
+  //   `CLIENT_DISCONNECTED : MQTT Client ${
+  //     client ? client.id : client
+  //   } disconnected from the aedes broker ${aedes.id}`
+  // );
+  const clientId = client.id;
+  aedes.publish({ topic: `gps/${clientId}`, payload: 'Disconnected' });
+  console.log('> Client disconnected');
 });
 // emitted when a client subscribes to a message topic
 aedes.on('subscribe', function (subscriptions, client) {
-  console.log(
-    `TOPIC_SUBSCRIBED : MQTT Client ${
-      client ? client.id : client
-    } subscribed to topic: ${subscriptions
-      .map((s) => s.topic)
-      .join(',')} on aedes broker ${aedes.id}`
-  );
+  // console.log(
+  //   `TOPIC_SUBSCRIBED : MQTT Client ${
+  //     client ? client.id : client
+  //   } subscribed to topic: ${subscriptions
+  //     .map((s) => s.topic)
+  //     .join(',')} on aedes broker ${aedes.id}`
+  // );
+  console.log('> Client subscribed');
 });
 // emitted when a client unsubscribes from a message topic
 aedes.on('unsubscribe', function (subscriptions, client) {
-  console.log(
-    `TOPIC_UNSUBSCRIBED : MQTT Client ${
-      client ? client.id : client
-    } unsubscribed to topic: ${subscriptions.join(',')} from aedes broker ${
-      aedes.id
-    }`
-  );
+  // console.log(
+  //   `TOPIC_UNSUBSCRIBED : MQTT Client ${
+  //     client ? client.id : client
+  //   } unsubscribed to topic: ${subscriptions.join(',')} from aedes broker ${
+  //     aedes.id
+  //   }`
+  // );
+  console.log('> Client unsubscribed');
 });
 // emitted when a client publishes a message packet on the topic
 aedes.on('publish', function (packet, client) {
   if (client) {
-    console.log(
-      `MESSAGE_PUBLISHED : MQTT Client ${
-        client ? client.id : 'AEDES BROKER_' + aedes.id
-      } has published message "${packet.payload}" on ${
-        packet.topic
-      } to aedes broker ${aedes.id}`
-    );
+    // console.log(
+    //   `MESSAGE_PUBLISHED : MQTT Client ${
+    //     client ? client.id : 'AEDES BROKER_' + aedes.id
+    //   } has published message "${packet.payload}" on ${
+    //     packet.topic
+    //   } to aedes broker ${aedes.id}`
+    // );
+
+    console.log('> Client published');
   }
 });
